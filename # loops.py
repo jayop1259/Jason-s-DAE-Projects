c5a793b0a0ab73336ff30c7475fa633926639f7e
jayop129 <jasonortega1110@gmail.com>
2024-10-17 17:16:45 -0400

    first commit

---
+++ # loops.py
@@ -0,0 +1,22 @@
+# loops
+
+print('hello world')
+
+
+# for loops
+number_of_times = 5
+#plain english
+
+for index in range(5): 
+     #print('hello world')
+     print(index)
+
+# while loops 
+cond = 0
+
+while cond >= 5: 
+     print ('hello world')
+     cond = cond + 1
+     print (cond)
+     
+ 
