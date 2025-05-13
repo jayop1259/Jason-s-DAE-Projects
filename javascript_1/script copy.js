ee32ab060e321d115052d84dbb4264fcc1146309
jayop1259 <jasonortega1110@gmail.com>
2025-05-13 17:52:50 -0400

    added javascript

---
+++ javascript_1/script copy.js
@@ -0,0 +1,11 @@
+alert("Hello")
+let age = 16
+if (age>=16){
+    console.log('You are a teenager');
+}
+else{
+    console.log('Be positive!')
+}
+const birthYear= 2008;
+
+console.log("My birth year is ", birthYear)
